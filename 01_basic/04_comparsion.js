86d6b8e2524e67fb444a3b16aa628c2cdca9db64
Muhammad Afaq <99785616+Afaq503@users.noreply.github.com>
2024-02-06 16:45:24 +0000

    comparsion

---
+++ 01_basic/04_comparsion.js
@@ -0,0 +1,16 @@
+console.log(2>1);
+console.log(2>=1);
+
+// clean cod
+console.log("2">=1);
+console.log("02">=1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+
+// ===
+console.log("2" === 0);
+

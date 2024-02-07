53e4cba3fcb382822d67ffff3cb63a2db7a48023
Muhammad Afaq <99785616+Afaq503@users.noreply.github.com>
2024-02-07 10:38:03 +0000

    Object and Json formate

--- 02_basic/02_Object.js
+++ 02_basic/02_Object.js
@@ -28,9 +28,95 @@ Jsuser.email = 'aftab@chatgpt.yahoo'
 Jsuser.greeting = function(){
     console.log("Heloo Jsuser");
 }
-console.log(Jsuser.greeting())
+// console.log(Jsuser.greeting())
 
 Jsuser.greeting2 = function(){
     console.log(`Hello Js User, ${this.name}`);
 }
-console.log(Jsuser.greeting2())
+// console.log(Jsuser.greeting2())
+// =============================================================
+// this is a singleton object
+// const foodpanda = new Object()
+
+const foodpanda = {}
+
+foodpanda.id="123"
+foodpanda.name='Afaq'
+foodpanda.isLoggedIn= false
+
+// console.log(foodpanda);
+
+const regularUser = {
+    email: 'some@gmail.com',
+    fullname: {
+        userfullname:{
+            firstname:"afaq",
+            lastname:'Gujjar'
+        }
+    }
+}
+
+
+// console.log(regularUser.fullname.userfullname.firstname);
+const obj1 = {
+    1:"a",
+    2:'b'
+}
+const obj2 = {
+    3:"c",
+    4:'d'
+}
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1, ...obj2} // spread oprater
+// console.log(obj3);
+const  users = [
+    {
+        id:1,
+        email:'a@gmail.com'
+    },
+    {
+        id:2,
+        email:'b@gmail.com'
+    },
+    {
+        id:3,
+        email:'c@gmail.com'
+    },
+]
+// console.log(users[1].id);
+// console.log(foodpanda);
+
+// console.log(Object.keys(foodpanda));
+// console.log(Object.values(foodpanda));
+// console.log(Object.entries(foodpanda));
+
+// console.log(foodpanda.hasOwnProperty('isLogged'));
+
+// ===============================================================
+
+// Object desturctureing
+
+const course = {
+    course: "Js is Urdu",
+    price: "999",
+    courseAuther: "Afaq"
+}
+
+// course.courseAuther
+const {courseAuther: auther} = course
+console.log(auther);
+// DeSturcture in React 
+
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "afaq")
+
+// js formate
+
+// {
+//     "name": "afaq",
+//     "coursename":'js in Urdu',
+//     "price":"free,"
+// }

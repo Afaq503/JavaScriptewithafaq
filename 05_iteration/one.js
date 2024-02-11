04c23194c01d6870d1becd02152fea141f86d2d6
Muhammad Afaq <99785616+Afaq503@users.noreply.github.com>
2024-02-11 07:47:30 +0000

    forloopiteration

---
+++ 05_iteration/one.js
@@ -0,0 +1,50 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log(`${element} is best number`);
+    }
+    // console.log(element);
+    
+}
+
+// console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+ for (let j = 1; j <= 10; j++) {    
+    // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+
+    // console.log(i + '*' + j + '=' + i*j);
+ }   
+}
+
+let myArray = ["batman", "superman", "flash"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+//  key words
+//  break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        // console.log(`${index} is best value detacted`);
+        break
+    }
+
+    // console.log(`Value of is is : ${index}`);
+}
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`${index} is best value detacted`);
+        continue
+    }
+
+    console.log(`Value of is is : ${index}`);
+}
